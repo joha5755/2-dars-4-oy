c49bb6395a86c8f937b75b06c92f7ef11acf765e
j <j>
2024-12-16 15:10:31 +0500

    aaaa

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
 // masala-1
-const A = 5;
 
-if (A > 0) {
-    console.log("Jumla rost: A soni musbat.");
-} else {
-    console.log("Jumla yolg'on: A soni musbat emas.");
-}
+// let A = 21;
+
+// if (A > 0) {
+//     console.log(` rost: ${A} soni musbat.`);
+// } 
 
 // Masala-2
 
-// let s = 7;
+// let A = -6;
 
-// if (s % 2 !== 0) {
-//     console.log("Jumla rost: A soni toq son.");
-// } else {
-//     console.log("Jumla yolg'on: A soni toq son emas (juft son).");
+// if (A < 0) {
+//     console.log(`kiritilgan son musbat emas.`);
 // }
 
 // Masala-3
 
 // let k = 5; 
-// let B = 3; 
+// let r = 3; 
 
-// if (k > 2 && B <= 3) {
-//     console.log("Jumla rost: A > 2 va B <= 3.");
+// if (k > 2 && r <= 3) {
+//     console.log(" rost");
 // } else {
-//     console.log("Jumla yolg'on: A > 2 va B <= 3 emas.");
+//     console.log(" yolgon");
 // }
 
 // Masala-4
 
-// let r = 3; 
-// let v = 5; 
-// let C = 7; 
+// let A = 2;
+// let B = 8;
+// let C = 5;
 
-// if (A <= v && v <= C) {
-//     console.log("Jumla rost: A <= B <= C.");
+// if (A <= B && B < C) {
+//     console.log("rost");
 // } else {
-//     console.log("Jumla yolg'on: A <= B <= C emas.");
+//     console.log("yolg'on");
 // }
 
 // Masala-5
@@ -46,9 +43,9 @@ if (A > 0) {
 // let f = 8; 
 
 // if ((t % 2 === 0 && f % 2 === 0) || (t % 2 !== 0 && f % 2 !== 0)) {
-//     console.log("Jumla rost: A va B sonlarining har ikkalasi ham yoki toq yoki juft son.");
+//     console.log("rost");
 // } else {
-//     console.log("Jumla yolg'on: A va B sonlarining har ikkalasi ham yoki toq yoki juft son emas.");
+//     console.log("yolgon");
 // }
 
 // Masala-6
@@ -58,76 +55,52 @@ if (A > 0) {
 // let m = 5;  
 
 // if (x > 0 || n > 0 || m > 0) {
-//     console.log("Jumla rost: A, B, C sonlarning hech bo'lmaganda bittasi musbat.");
+//     console.log("Jumla rost A, B, C sonlarning bittasi musbat");
 // } else {
-//     console.log("Jumla yolg'on: A, B, C sonlarning hech biri musbat emas.");
+//     console.log("Jumla yolgon A, B, C sonlarning musbat emas");
 // }
 
 // masala-7
 
-// let number = 123; // Masalan, 123
+// let number = 24;
 
-// if (number >= 100 && number <= 999) {
-//     let digits = String(number);
-//     let firstDigit = digits[0];
-//     let secondDigit = digits[1];
-//     let thirdDigit = digits[2];
-
-//     if (firstDigit !== secondDigit && firstDigit !== thirdDigit && secondDigit !== thirdDigit) {
-//         console.log("Rost");
-//     } else {
-//         console.log("Yolg'on");
-//     }
+// if (number >= 10 && number <= 99 && number % 2 === 0) {
+//     console.log("rost juft son.");
 // } else {
-//     console.log("Iltimos, uch xonali son kiriting.");
+//     console.log("yolgon juft son emas.");
 // }
 
 // masala-8
 
-// let A = prompt("A sonini kiriting:");
-// let B = prompt("B sonini kiriting:");
-// let C = prompt("C sonini kiriting:");
+// let number = 135; 
 
-// A = parseInt(A);
-// B = parseInt(B);
-// C = parseInt(C);
-
-// if (A === B || B === C || A === C) {
-//   console.log(
-//     "Berilgan uchta butun sonlarning hech bo'lmaganda 2 tasi bir biriga teng: Rost."
-//   );
+// if (number >= 100 && number <= 999 && number % 2 !== 0) {
+//     console.log("Rrost toq son");
 // } else {
-//   console.log(
-//     "Berilgan uchta butun sonlarning hech bo'lmaganda 2 tasi bir biriga teng: Yolg'on."
-//   );
+//     console.log("Yolgon");
 // }
 
 
 // masala-9
 
-// let x1 = 4, y1 = 4; // Masalan, (4, 4)
-// let x2 = 6, y2 = 6; // Masalan, (6, 6)
+// let A = 5;  
+// let B = 7;  
+// let C = 10; 
 
-// // Koordinatalarning 1-8 oraliqda yotganligini tekshirish
-// if (x1 >= 1 && x1 <= 8 && y1 >= 1 && y1 <= 8 && x2 >= 1 && x2 <= 8 && y2 >= 1 && y2 <= 8) {
-//     // Farzinning yurishi qoidalarini tekshirish
-//     if (x1 === x2 || y1 === y2 || Math.abs(x1 - x2) === Math.abs(y1 - y2)) {
-//         console.log("Rost");
-//     } else {
-//         console.log("Yolg'on");
-//     }
+// if ((B > A && B < C) || (B < A && B > C)) {
+//     console.log(" a soni b va C sonlari orasida yotadi");
 // } else {
-//     console.log("Koordinatalar 1-8 oraliqda bo'lishi kerak.");
+//     console.log(" yolgon B soni A va C sonlari orasida yotmaydi");
 // }
 
 
 // masala-10
 
+// let A = 5;  
+// let B = 4;  
 
-// let number = 24;
-
-// if (number >= 10 && number <= 99 && number % 2 === 0) {
-//     console.log("Rost");
+// if ((A % 2 !== 0 && B % 2 === 0) || (A % 2 === 0 && B % 2 !== 0)) {
+//     console.log("Rost A va B sonlarning faqat bittasi toq son");
 // } else {
-//     console.log("Yolg'on");
+//     console.log("Yolgon A va B sonlarning faqat bittasi toq son emas");
 // }
